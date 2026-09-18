36b7ea0fe9a97b5abb697aa8a44a8353df9555ec
AkasDatta <012akasdatta@gmail.com>
2026-09-19 02:01:31 +0600

    modified all file and folder

---
+++ src/Pages/Home/LandingPage/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LandingPage = () => {
+  return <div>dddd</div>;
+};
+
+export default LandingPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Router from "./App";
+import { RouterProvider } from "react-router/dom";
+import { router } from "./Router/Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
